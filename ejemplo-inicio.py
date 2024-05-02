9fe5ac7cc01484b2ca4565b8d052bdd5a60c6d1f
Aventurinna <aidi@aidi.com>
2024-05-02 20:25:49 +0200

    ejemplo 3d básico
    Este es un ejemplo de script con API BLENDER PYTHON

---
+++ ejemplo-inicio.py
@@ -0,0 +1,8 @@
+import bpy
+import random as r
+
+bpy.ops.object.select_all(action='SELECT')
+bpy.ops.object.delete(use_global=False)
+
+for i in range(100):
+    bpy.ops.mesh.primitive_cube_add(size=2, enter_editmode=False, align='WORLD', location=(r.random(),r.random(),r.random()), scale=(.1,.1,.1))
