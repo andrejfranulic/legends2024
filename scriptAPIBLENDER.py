d7d948ec19565f401e590390c38fd95ae7437a9a
Aventurinna <aidi@aidi.com>
2024-05-09 20:44:36 +0200

    Update scriptAPIBLENDER.py

--- scriptAPIBLENDER.py
+++ scriptAPIBLENDER.py
@@ -1,26 +1,39 @@
 import bpy
-
+import random as r
 class SimpleOperator(bpy.types.Operator):
     """Tooltip"""
-    bl_idname = "object.simple_operator"
-    bl_label = "Mi botón"
+    bl_idname = "object.s_p"
+    bl_label = "Crear"
 
     def execute(self, context):
         # Aquí va la lógica de tu operador
-        context.object.location.z += 1.0
+        for i in range(10):
+            bpy.ops.mesh.primitive_cube_add(size=2, enter_editmode=False, align='WORLD', location=(r.random(),r.random(),r.random()), scale=(.1,.1,.1))
+        return {'FINISHED'}
+
+class SimpleOperator2(bpy.types.Operator):
+    """Tooltip"""
+    bl_idname = "object.s_p"
+    bl_label = "Borrar"
+
+    def execute(self, context):
+        # Aquí va la lógica de tu operador
+        for i in range(10):
+            bpy.ops.mesh.primitive_cube_add(size=2, enter_editmode=False, align='WORLD', location=(r.random(),r.random(),r.random()), scale=(.1,.1,.1))
         return {'FINISHED'}
     
 class SimplePanel(bpy.types.Panel):
     """Crea un Panel en la región de la barra de herramientas"""
-    bl_label = "Mi Panel"
+    bl_label = "Mi etiqueta"
     bl_idname = "OBJECT_PT_simple"
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
-    bl_category = "Mi Pestaña"
+    bl_category = "Mi Panel"
 
     def draw(self, context):
         layout = self.layout
-        layout.operator(SimpleOperator.bl_idname, icon='WORLD_DATA')
+        layout.operator(SimpleOperator.bl_idname, icon='GHOST_ENABLED')
+        layout.operator(SimpleOperator2.bl_idname, icon='GHOST_DISABLED')
 
 def register():
     bpy.utils.register_class(SimplePanel)
